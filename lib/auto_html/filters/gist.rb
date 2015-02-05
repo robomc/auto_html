76810a0efa5420ce3d40614a9ce12fde8c92d07b
robomc <rob.mcgrail@gmail.com>
2015-02-05 16:57:00 +1300

    Fixing broken gist ID regex
    Gist IDs are a hash, which can include letters and numbers.

--- lib/auto_html/filters/gist.rb
+++ lib/auto_html/filters/gist.rb
@@ -1,6 +1,6 @@
 AutoHtml.add_filter(:gist).with({}) do |text, options|
   # E.g. https://gist.github.com/1710276
-  regex = %r{https?://gist\.github\.com/(\w+/)?(\d+)}
+  regex = %r{https?://gist\.github\.com/(\w+/)?(\w+)}
   text.gsub(regex) do
     gist_id = $2
     %{<script type="text/javascript" src="https://gist.github.com/#{gist_id}.js"></script>}
